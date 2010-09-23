1a6a8c94fc6f3173c2c50bc43d29840261200d13
Tiago <tiago@MacBook-Pro.local>
2010-09-23 23:01:31 +0200

    Splitting creation of arrays and trees.

--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -5,33 +5,44 @@ require "stopwatch"
 
 describe Search do
   
-  before :each do
+  before :all do
     @search_item = 182
-    @unsorted_array = DataSet.random_array(1000000, @search_item)
-    @sorted_array = @unsorted_array.sort
   end
   
   it "should perform linear search" do
+    setup_arrays
     search_and_check Search::LinearSearch.new, @unsorted_array
   end
   
   it "should perform binary search" do
+    setup_arrays
     search_and_check Search::BinarySearch.new, @sorted_array
   end
   
-  it "should perform breadth-first search" do
-    # TODO DataSet.random_binary_tree(1000000)
-  end
-
   it "should perform depth-first search" do
-    # TODO DataSet.random_binary_tree(1000000)
+    setup_trees
+    # TODO
   end
 
-  def search_and_check(algorithm, array)
+  it "should perform breadth-first search" do
+    setup_trees
+    # TODO
+  end
+
+  def search_and_check(algorithm, data_structure)
     puts "\nRunning #{algorithm.class.name}..."
     StopWatch.start
-    algorithm.search(array, @search_item).should_not == -1
+    algorithm.search(data_structure, @search_item).should_not == -1 || false
     StopWatch.stop
+  end
+
+  def setup_arrays
+    @unsorted_array = DataSet.random_array(1000000, @search_item)
+    @sorted_array = @unsorted_array.sort # Quick sort
+  end
+
+  def setup_trees
+    @binary_tree = DataSet.random_binary_tree(100000, @search_item)
   end
 
 end
